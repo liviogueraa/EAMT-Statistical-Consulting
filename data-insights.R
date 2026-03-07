abf0236d5441832fc099dd1ffd70f6d36ede75fe
liviogueraa <livio.guerra02@gmail.com>
2026-03-07 11:18:37 +0100

    Added Boxplots of Time, Keystrokes in relation to Condition

--- data-insights.R
+++ data-insights.R
@@ -1,10 +1,40 @@
-####################################################################
-# - THIS SECTION IS DEDICATED TO A PRIOR ANALYSIS OF THE DATASET - #
-####################################################################
+############################################################################
+# ----- THIS SECTION IS DEDICATED TO A PRIOR ANALYSIS OF THE DATASET ----- #
+############################################################################
 
 # Required Packages
-
 library(readr)
+library(ggplot2)
+library(dplyr)
+library(see)
+library(patchwork)
 
 # Loading dataset
 data <- read_csv("data/data.csv")
+
+# Tables  
+table(data$condition)
+table(data$domain)
+
+# Figures
+timebp <- ggplot(data = data, mapping = aes(x = as.factor(condition),
+                 y = time, fill = as.factor(condition))) + 
+  geom_boxplot() +
+  theme_minimal() +
+  scale_fill_okabeito(order = 3:6) +
+  labs(title = "Temporal Efficiency per Condition",
+       x = "Condition",
+       y = "Time (seconds)") +
+  theme(legend.position = "none")
+
+keybp <- ggplot(data = data, mapping = aes(x = as.factor(condition), 
+                y = keystrokes, fill = as.factor(condition))) + 
+  geom_boxplot() +
+  theme_minimal() +
+  scale_fill_okabeito(order = 3:6) +
+  labs(title = "Number of Keystrokes per Condition",
+       x = "Condition",
+       y = "Number of Keystrokes") +
+  theme(legend.position = "none")
+
+timebp/keybp
