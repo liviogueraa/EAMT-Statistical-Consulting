07145a382fc953838348cf37e6ba1ebac6205161
liviogueraa <livio.guerra02@gmail.com>
2026-03-07 10:49:19 +0100

    10:49 - First commit test

---
+++ data-insights.R
@@ -0,0 +1,10 @@
+####################################################################
+# - THIS SECTION IS DEDICATED TO A PRIOR ANALYSIS OF THE DATASET - #
+####################################################################
+
+# Required Packages
+
+library(readr)
+
+# Loading dataset
+data <- read_csv("data/data.csv")
