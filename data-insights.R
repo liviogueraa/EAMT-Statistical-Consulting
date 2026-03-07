fae3baf7cf4577e8d06479fa8d94472788eade54
liviogueraa <livio.guerra02@gmail.com>
2026-03-07 11:18:37 +0100

    Added Boxplots of Time, Keystrokes in relation to Condition

--- data-insights.R
+++ data-insights.R
@@ -1,10 +1,12 @@
-####################################################################
-# - THIS SECTION IS DEDICATED TO A PRIOR ANALYSIS OF THE DATASET - #
-####################################################################
+############################################################################
+# ----- THIS SECTION IS DEDICATED TO A PRIOR ANALYSIS OF THE DATASET ----- #
+############################################################################
 
 # Required Packages
-
 library(readr)
+library(ggplot2)
+library(dplyr)
 
 # Loading dataset
 data <- read_csv("data/data.csv")
+
