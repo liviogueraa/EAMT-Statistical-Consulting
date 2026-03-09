685e2e462cf647308bc4cfc3edc1fab9277581e3
liviogueraa <livio.guerra02@gmail.com>
2026-03-09 11:48:34 +0100

    Factorization and Ordering of categorical variables

--- data-insights.R
+++ data-insights.R
@@ -14,15 +14,58 @@ library(patchwork)
 data <- read_csv("data/data.csv")
 
 #=======
-# Factorization of categorical variables
-data_categorical <- data[,-c(7,8,9,10,11,12,13)]
+# Factorization and Ordering of categorical variables
 
-data_categorical[] <- lapply(data_categorical, as.factor)
-sapply(data_categorical, unique, simplify = FALSE)
+data$`How good was the quality of MT?` <- factor(
+  data$`How good was the quality of MT?`, 
+  levels = c("Insufficient", "Fair", "Good", "Very good"),
+  ordered = TRUE
+)
+
+data$`How difficult to translate was the source text?` <- factor(
+  data$`How difficult to translate was the source text?`, 
+  levels = c("Very easy", "Somewhat easy", "Neutral",
+             "Somewhat difficult", "Very difficult"),
+  ordered = TRUE
+)
+
+data$`How useful were the error annotations?` <- factor(
+  data$`How useful were the error annotations?`, 
+  levels = c("nan", "Extremely not useful", "Somewhat not useful",
+             "Neutral", "Somewhat useful", "Extremely useful"),
+  ordered = TRUE
+)
+
+data$`How useful were the error annotations?` <- factor(
+  data$`How useful were the error annotations?`, 
+  levels = c("nan", "Extremely not useful", "Somewhat not useful",
+             "Neutral", "Somewhat useful", "Extremely useful"),
+  ordered = TRUE
+)
+
+data$`How accurate were the error annotations?` <- factor(
+  data$`How accurate were the error annotations?`, 
+  levels = c("nan", "Somewhat inaccurate", "Not accurate nor inaccurate",
+             "Somewhat accurate"),
+  ordered = TRUE
+)
+
+data$`How useful were the translation suggestions?` <- factor(
+  data$`How useful were the translation suggestions?`, 
+  levels = c("nan", "Neutral", "Somewhat useful",
+             "Extremely useful"),
+  ordered = TRUE
+)
+
+data$`How accurate were the translation suggestions?` <- factor(
+  data$`How accurate were the translation suggestions?`, 
+  levels = c("nan", "Somewhat inaccurate", "Not accurate nor inaccurate",
+             "Somewhat accurate", "Very accurate"),
+  ordered = TRUE
+)
 
 # Creating tables per variable
-# The corrected error "tags_edited" is removed, for simplicity of visualization
-
+data_categorical <- data[,-c(7,8,9,10,11,12,13)]
 lapply(data_categorical[,-7], table)
 
 #=======
@@ -33,14 +76,21 @@ table(data$PET, data$`How useful were the translation suggestions?`)
 table(data$PET, data$`How good was the quality of MT?`)
 table(data$`How good was the quality of MT?`, data$condition)
 table(data$text_name, data$`How good was the quality of MT?`)
-table(data$condition, data$`How difficult to translate was the source text?`)
-table(data$condition, data$`How good was the quality of MT?`)
 
 data %>%
   group_by(`How good was the quality of MT?`) %>%
   summarise(mean_time = mean(time, na.rm = TRUE),
             mean_keys = mean(keystrokes, na.rm = TRUE),
             n = n())
+
+data %>%
+  group_by(condition) %>%
+  summarise(mean_time = mean(time),
+            mean_keys = mean(keystrokes),
+            n = n())
+
+table(data$condition, data$`How good was the quality of MT?`)
+table(data$condition, data$`How difficult to translate was the source text?`)
 
 table(data$condition, data$num_major)
 table(data$condition, data$num_minor)
@@ -99,7 +149,7 @@ keybp_faceted <- ggplot(data = data, aes(x = as.factor(condition), y = keystroke
   theme_minimal() +
   scale_fill_okabeito(order = 3:6) + 
   
-  labs(title = "Keystrokes per Condition",
+  labs(title = "Number of Keystrokes per Condition",
        subtitle = "Divided by Professional Translator",
        x = "Condition",
        y = "Number of Keystrokes") +
